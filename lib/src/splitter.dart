0e9d4b7bef79ca05ba30b07cecdf7de29b50b2c3
Nikita Timkov <nikita.timkov1997@gmail.com>
2021-03-31 01:06:24 +0300

    Implement hoverable effect on splitter widget

--- lib/src/splitter.dart
+++ lib/src/splitter.dart
@@ -8,7 +8,7 @@ import 'package:splittable_view/src/theme.dart';
 import 'package:splittable_view/src/extensions.dart';
 
 /// Widget which splits two or more widgets in [SplittableView].
-class Splitter extends StatelessWidget {
+class Splitter extends StatefulWidget {
   /// Creates a splitter widget.
   const Splitter({
     Key? key,
@@ -19,20 +19,30 @@ class Splitter extends StatelessWidget {
   final Axis direction;
 
   @override
+  _SplitterState createState() => _SplitterState();
+}
+
+class _SplitterState extends State<Splitter> {
+  /// Contains the current hover color.
+  Color? hoverColor;
+
+  @override
   Widget build(BuildContext context) {
     var splitterTheme = SplitterTheme.of(context);
-    var splitterSize = direction.isHorizonal
+    var splitterSize = widget.direction.isHorizonal
         ? Size.fromHeight(splitterTheme.space!)
         : Size.fromWidth(splitterTheme.space!);
     var iconSize = IconTheme.of(context).size;
-    return Container(
+    // Create base splitter
+    Widget splitter = Container(
+      color: hoverColor,
       alignment: Alignment.center,
       height: splitterSize.height,
       width: splitterSize.width,
       child: Stack(
         children: [
           // Horizontal divider
-          if (direction.isHorizonal)
+          if (widget.direction.isHorizonal)
             Divider(
               color: splitterTheme.color,
               thickness: splitterTheme.thickness,
@@ -40,7 +50,7 @@ class Splitter extends StatelessWidget {
               endIndent: splitterTheme.endIndent,
             ),
           // Vartical divider
-          if (direction.isVertical)
+          if (widget.direction.isVertical)
             VerticalDivider(
               color: splitterTheme.color,
               thickness: splitterTheme.thickness,
@@ -50,7 +60,7 @@ class Splitter extends StatelessWidget {
           // Icon
           if (splitterTheme.enableIcon!)
             () {
-              var rect = direction.isHorizonal
+              var rect = widget.direction.isHorizonal
                   ? Rect.fromLTRB(0.0,
                       (iconSize! - splitterTheme.space!) / -2 - 0.5, 0.0, 0.0)
                   : Rect.zero;
@@ -58,7 +68,7 @@ class Splitter extends StatelessWidget {
                 Icons.drag_handle,
                 color: splitterTheme.color,
               );
-              if (direction.isVertical) {
+              if (widget.direction.isVertical) {
                 icon = RotationTransition(
                   turns: const AlwaysStoppedAnimation(0.25),
                   child: icon,
@@ -77,5 +87,22 @@ class Splitter extends StatelessWidget {
         ],
       ),
     );
+    // Add hover effect
+    if (splitterTheme.hoverable!) {
+      splitter = MouseRegion(
+        onEnter: (details) {
+          setState(() {
+            hoverColor = splitterTheme.hoverColor;
+          });
+        },
+        onExit: (details) {
+          setState(() {
+            hoverColor = null;
+          });
+        },
+        child: splitter,
+      );
+    }
+    return splitter;
   }
 }
